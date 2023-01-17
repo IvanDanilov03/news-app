b48a97fdef7fe910776951e571cd889a5ae1187c
IvanDanilov03 <ivan_danilov@ukr.net>
2023-01-17 18:04:32 +0200

    feat: create NewsDetailsPage

---
+++ src/pages/NewsDetailsPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Typography } from "../components/ui/Typography";
+
+const NewsDetailsPage: React.FC = () => {
+  return <Typography>NewsDetailsPage</Typography>;
+};
+
+export default NewsDetailsPage;
