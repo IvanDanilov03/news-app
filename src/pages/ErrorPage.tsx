29d7238b764d3c916dea2dcd05164d9c7d83c316
IvanDanilov03 <ivan_danilov@ukr.net>
2023-01-17 18:04:42 +0200

    feat: create ErrorPage

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import { Box } from "../components/ui/Box";
+import { Typography } from "../components/ui/Typography";
+
+const ErrorPage: FC = () => {
+  return (
+    <Box>
+      <Typography>Error Page</Typography>
+    </Box>
+  );
+};
+
+export default ErrorPage;
