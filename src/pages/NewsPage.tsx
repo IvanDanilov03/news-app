ac0c65b8639ea502701c2e03d959986f959a324d
IvanDanilov03 <ivan_danilov@ukr.net>
2023-01-17 18:04:20 +0200

    feat: create NewsPage

---
+++ src/pages/NewsPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Typography } from "../components/ui/Typography";
+
+const NewsPage: React.FC = () => {
+  return <Typography variant="tiny">NewsPage</Typography>;
+};
+
+export default NewsPage;
