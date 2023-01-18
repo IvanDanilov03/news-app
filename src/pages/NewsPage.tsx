d10fafb0ed759669ed6375712a462f5283c388ab
IvanDanilov03 <ivan_danilov@ukr.net>
2023-01-18 11:07:28 +0200

    feat: add fetching data to NewsPage

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -1,8 +1,59 @@
-import React from "react";
+import { Button } from "@mui/material";
+import React, { FC, useCallback, useEffect, useState } from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { Box } from "../components/ui/Box";
 import { Typography } from "../components/ui/Typography";
 
-const NewsPage: React.FC = () => {
-  return <Typography variant="tiny">NewsPage</Typography>;
+export interface News {
+  id: number;
+  title: string;
+  url: string;
+  imageUrl: string;
+  newsSite: string;
+  summary: string;
+  publishedAt: string;
+  updatedAt: string;
+  featured: boolean;
+  launches: string[];
+  events: string[];
+}
+
+const NewsPage: FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedNews, setLoadedNews] = useState<News[]>([]);
+  const [httpError, setHttpError] = useState("");
+
+  const fetchNews = useCallback(async () => {
+    setIsLoading(true);
+    setHttpError("");
+    try {
+      const response = await fetch(
+        "https://api.spaceflightnewsapi.net/v3/articles"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseData = await response.json();
+
+      setLoadedNews(responseData);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setHttpError((error as Error).message);
+    }
+  }, [setIsLoading, setHttpError]);
+
+  useEffect(() => {
+    fetchNews();
+  }, [fetchNews]);
+
+  return (
+    <Box>
+      <Typography variant="tiny">NewsPage</Typography>
+    </Box>
+  );
 };
 
 export default NewsPage;
