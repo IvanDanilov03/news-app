0607facd00ca3fde2c2d13ce94e6ed53bff7dbff
IvanDanilov03 <ivan_danilov@ukr.net>
2023-01-19 17:56:20 +0200

    feat: create ArticlePageView

--- src/pages/ArticlePageView/styles.ts
+++ src/pages/ArticlePageView/styles.ts
@@ -1,10 +1,42 @@
 import { SxStyles } from "../../types/styles";
+import { basicTheme } from "../../theme";
 
 export const styles: SxStyles = {
   container: {
     display: "flex",
     flexWrap: "wrap",
     justifyContent: "center",
-    mb: 10,
+    mb: "45px",
+  },
+  textBox: {
+    mx: "35px",
+    mt: "-150px",
+    p: 6,
+    border: "1px solid",
+    borderColor: "#EAEAEA",
+    zIndex: 2,
+    backgroundColor: "#FFFFFF",
+    maxWidth: "1290px",
+    [basicTheme.breakpoints.up("tablet")]: {
+      px: "75px",
+      pt: "35px",
+      pb: "50px",
+      mx: "75px",
+    },
+  },
+  title: {
+    textAlign: "center",
+    mb: "50px",
+  },
+  summary: {
+    mb: 4,
+  },
+  buttonWrapper: {
+    ml: "60px",
+    mr: "auto",
+    mt: "35px",
+    [basicTheme.breakpoints.up("tablet")]: {
+      ml: "150px",
+    },
   },
 };
