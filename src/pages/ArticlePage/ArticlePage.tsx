3101f527708731d1e71c593c7a6b180d58099e47
IvanDanilov03 <ivan_danilov@ukr.net>
2023-01-19 17:56:56 +0200

    feat: add ArticlePageView to ArticlePage and fetch data for it

--- src/pages/ArticlePage/ArticlePage.tsx
+++ src/pages/ArticlePage/ArticlePage.tsx
@@ -1,17 +1,56 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { FC, useCallback, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { Box } from "../../components/ui/Box";
-import { Typography } from "../../components/ui/Typography";
-import { GoButton } from "../../components/unsorted/GoButton";
+import { ArticlePageView } from "../ArticlePageView/ArticlePageView";
+import { News } from "../../types/api";
 
-
-const ArticlePage: React.FC = () => {
+const ArticlePage: FC = () => {
   const { newsId } = useParams();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedNewsDetails, setLoadedNewsDetails] = useState<News>();
+  const [httpError, setHttpError] = useState("");
+  const navigate = useNavigate();
+
+  const fetchNewsDetails = useCallback(async () => {
+    setIsLoading(true);
+    setHttpError("");
+    try {
+      const response = await fetch(
+        `https://api.spaceflightnewsapi.net/v3/articles/${newsId}`
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseData = await response.json();
+
+      setLoadedNewsDetails(responseData);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setHttpError((error as Error).message);
+    }
+  }, [setIsLoading, setHttpError, newsId]);
+
+  useEffect(() => {
+    fetchNewsDetails();
+  }, [fetchNewsDetails]);
+
+  if (typeof loadedNewsDetails === "undefined") {
+    navigate("/home");
+    return null;
+  }
 
   return (
     <Box>
-      <Typography>{newsId}</Typography>
-      <GoButton id={45654} text="Back to homepage" type="back" />
+      <ArticlePageView
+        title={loadedNewsDetails.title}
+        imageUrl={loadedNewsDetails.imageUrl}
+        newsSite={loadedNewsDetails.newsSite}
+        summary={loadedNewsDetails.summary}
+      />
     </Box>
   );
 };
