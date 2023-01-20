5b6a8b05af0a6f5ab70671c146f7652c5b242436
IvanDanilov03 <ivan_danilov@ukr.net>
2023-01-20 23:02:22 +0200

    feat: add content to HomePageView

--- src/pages/HomePageView/HomePageView.tsx
+++ src/pages/HomePageView/HomePageView.tsx
@@ -1,9 +1,17 @@
-import { FC } from "react";
+import Fuse from "fuse.js";
+import { FC, useEffect, useState } from "react";
+import { SearchIcon } from "../../components/icons/SearchIcon";
 import { Box } from "../../components/ui/Box";
 import { Container } from "../../components/ui/Container";
 import { Divider } from "../../components/ui/Divider";
+import { FormControl } from "../../components/ui/FormControl";
+import { Input } from "../../components/ui/Input";
+import { InputAdornment } from "../../components/ui/InputAdornment";
 import { Typography } from "../../components/ui/Typography";
 import { NewsList } from "../../components/unsorted/NewsList";
+import { News } from "../../types/api";
+import { useSearchParams } from "react-router-dom";
+import { IconButton } from "../../components/ui/IconButton";
 
 import { styles } from "./styles";
 
@@ -24,13 +32,95 @@ interface HomePageViewProps {
 }
 
 export const HomePageView: FC<HomePageViewProps> = ({ data }) => {
+  const [dataToRender, setDataToRender] = useState<News[]>([]);
+  const [inputText, setInputText] = useState("");
+  const [enterInput, setEnterInput] = useState(false);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const options = {
+    includeScore: true,
+    keys: [
+      {
+        name: "title",
+        weight: 2,
+      },
+      {
+        name: "summary",
+        weight: 1,
+      },
+    ],
+  };
+
+  const fuseNews = new Fuse<News>(data, options);
+  const resultFuseNews = fuseNews.search<News>(inputText);
+  const filterResultFuseNews = resultFuseNews.map(({ item }) => item);
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter" && inputText !== "") {
+      setSearchParams({ search: inputText });
+      setDataToRender(filterResultFuseNews);
+      setEnterInput(true);
+      if (inputText === "") {
+        setDataToRender(data);
+        setSearchParams();
+      }
+    }
+  };
+
+  const handleClick = () => {
+    setSearchParams({ search: inputText });
+    setDataToRender(filterResultFuseNews);
+    setEnterInput(true);
+  };
+
+  const queryParam = searchParams.get("search");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
+
+  useEffect(() => {
+    if (inputText === "" || queryParam === null) {
+      setDataToRender(data);
+      setSearchParams();
+      setEnterInput(false);
+    }
+  }, [inputText, data, queryParam, setSearchParams]);
+
   return (
     <Container sx={styles.root}>
+      <Box sx={styles.searchBox}>
+        <Typography variant="bodyLarge">Filter by keywords</Typography>
+        <FormControl sx={styles.search}>
+          <Input
+            sx={styles.searchIcon}
+            disableUnderline
+            startAdornment={
+              <InputAdornment position="start">
+                <IconButton onClick={handleClick}>
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            }
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+          />
+        </FormControl>
+      </Box>
+
       <Box sx={styles.resultBox}>
-        <Typography variant="bodyLarge">Result: {data.length}</Typography>
+        <Typography variant="bodyLarge">
+          Result: {dataToRender.length}
+        </Typography>
         <Divider sx={styles.divider} />
       </Box>
-      <NewsList data={data} />
+
+      <NewsList
+        data={dataToRender}
+        inputText={inputText}
+        enterInput={enterInput}
+      />
     </Container>
   );
 };
--- src/pages/HomePageView/styles.ts
+++ src/pages/HomePageView/styles.ts
@@ -1,4 +1,5 @@
 import { SxStyles } from "../../types/styles";
+import { basicTheme } from "../../theme";
 
 export const styles: SxStyles = {
   root: {
@@ -7,10 +8,44 @@ export const styles: SxStyles = {
   resultBox: {
     mt: "40px",
     mb: "45px",
+    mx: "50px",
+    [basicTheme.breakpoints.up("laptop")]: {
+      mx: 0,
+    },
   },
   divider: {
     width: "100%",
     backgroundColor: "#EAEAEA",
     mt: "5px",
+  },
+  red: {
+    color: "red",
+  },
+  textBox: {
+    mr: 1,
+  },
+  searchBox: {
+    mt: "50px",
+    mx: "50px",
+    [basicTheme.breakpoints.up("laptop")]: {
+      width: "600px",
+      mx: 0,
+    },
+  },
+  search: {
+    mt: "5px",
+    boxShadow: "0px 8px 24px rgba(0, 0, 0, 0.05)",
+    width: "100%",
+    borderRadius: "5px",
+    height: "50px",
+    [basicTheme.breakpoints.up("tablet")]: {
+      width: "300px",
+    },
+    [basicTheme.breakpoints.up("laptop")]: {
+      width: "600px",
+    },
+  },
+  searchIcon: {
+    my: "auto",
   },
 };
