4c0e0d457d92142bb7c015cac5c19f922b792fdd
IvanDanilov03 <ivan_danilov@ukr.net>
2023-01-20 13:50:12 +0200

    feat: create NewsList component

---
+++ src/components/unsorted/NewsList/index.tsx
@@ -0,0 +1,65 @@
+import { FC } from "react";
+
+import { styles } from "./styles";
+
+import { Box } from "../../ui/Box";
+import { NewsCard } from "../NewsCard";
+
+export interface NewsListProps {
+  data: {
+    id: number;
+    title: string;
+    url: string;
+    imageUrl: string;
+    newsSite: string;
+    summary: string;
+    publishedAt: string;
+    updatedAt: string;
+    featured: boolean;
+    launches: string[];
+    events: string[];
+  }[];
+}
+
+export const NewsList: FC<NewsListProps> = ({ data }) => {
+  const cleanDate = (createdAt: string) => {
+    const propcreatedDate = createdAt;
+    const [clearDate] = propcreatedDate.match(
+      /(\d{4})-(\d{1,2})-(\d{1,2})/
+    ) || [""];
+    const date = new Date(clearDate);
+    const monthName = date.toLocaleString("en", { month: "long" });
+    const year = date.toLocaleString("en", { year: "numeric" });
+    const dateNumber = date.toLocaleString("en", { day: "numeric" });
+
+    const dataArray = dateNumber.split("");
+    let dateName = "";
+    if (dataArray[dataArray.length - 1] === "1") {
+      dateName = `${dataArray.join("")}st`;
+    } else if (dataArray[dataArray.length - 1] === "2") {
+      dateName = `${dataArray.join("")}nd`;
+    } else if (dataArray[dataArray.length - 1] === "3") {
+      dateName = `${dataArray.join("")}rd`;
+    } else {
+      dateName = `${dataArray.join("")}th`;
+    }
+    const resultDate = `${monthName} ${dateName}, ${year}`;
+    return resultDate;
+  };
+
+  return (
+    <Box sx={styles.wrapper}>
+      {data.map((item) => (
+        <NewsCard
+          key={item.id}
+          dateTitle={cleanDate(item.publishedAt)}
+          newsTitle={item.title}
+          newsSummary={`${item.summary.slice(0, 100)}...`}
+          imageSrc={item.imageUrl}
+          imageAlt={item.title}
+          id={item.id}
+        />
+      ))}
+    </Box>
+  );
+};
---
+++ src/components/unsorted/NewsList/styles.ts
@@ -0,0 +1,10 @@
+import { SxStyles } from "../../../types/styles";
+
+export const styles: SxStyles = {
+  wrapper: {
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "45px",
+    justifyContent: "center",
+  },
+};
